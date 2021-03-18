88e471b63556af347fdf5c7174f771bf2623c620
Ivan Vasic <ivasic@gmail.com>
2021-03-18 10:41:25 +0100

    Update for Google Maps iOS SDK v 4.2.0

--- Package.swift
+++ Package.swift
@@ -41,28 +41,28 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "GoogleMaps",
-            url: "https://github.com/ivasic/google-maps-ios/releases/download/4.1.0/GoogleMaps.xcframework.zip",
-            checksum: "7ede7b57da17ad407ca62c67a0273d3be3c7cb67563c877d2fd1978e90056b54"
+            url: "https://github.com/ivasic/google-maps-ios/releases/download/4.2.0/GoogleMaps.xcframework.zip",
+            checksum: "0eced519b05a519123cb9cd17caba4d91b017b97dac062069d3f75972e9c37f5"
         ),
         .binaryTarget(
             name: "GoogleMapsBase",
-            url: "https://github.com/ivasic/google-maps-ios/releases/download/4.1.0/GoogleMapsBase.xcframework.zip",
-            checksum: "e6c6967accc7161e5e462196b0a9a5f4a52254cf5fbcb5cd85821f9e31ff9fb9"
+            url: "https://github.com/ivasic/google-maps-ios/releases/download/4.2.0/GoogleMapsBase.xcframework.zip",
+            checksum: "83cc08ebe0f942a215e9a636af52abf784ed141e4446d9662dd1a8f6bd78f79b"
         ),
         .binaryTarget(
             name: "GoogleMapsCore",
-            url: "https://github.com/ivasic/google-maps-ios/releases/download/4.1.0/GoogleMapsCore.xcframework.zip",
-            checksum: "5045c4f55e8852594083d4aa27bdc186f350a3d5cd8ebb6b0cd05a9e98bdd4cd"
+            url: "https://github.com/ivasic/google-maps-ios/releases/download/4.2.0/GoogleMapsCore.xcframework.zip",
+            checksum: "ac018d9addb352723682f7ed90c03ec3862d28b4f7977fa4366dc98d4f9e511b"
         ),
         .binaryTarget(
             name: "GoogleMapsM4B",
-            url: "https://github.com/ivasic/google-maps-ios/releases/download/4.1.0/GoogleMapsM4B.xcframework.zip",
-            checksum: "35ec9bc3c475bbe290bd6a1e3f43cd1962a98136c53aebfc01476ed814c140be"
+            url: "https://github.com/ivasic/google-maps-ios/releases/download/4.2.0/GoogleMapsM4B.xcframework.zip",
+            checksum: "d63a9db38d94595e31427810bc85428bafb69aa412e6deb4110d4c11289c1aad"
         ),
         .binaryTarget(
             name: "GooglePlaces",
-            url: "https://github.com/ivasic/google-maps-ios/releases/download/4.1.0/GooglePlaces.xcframework.zip",
-            checksum: "984649d78655876edb844e3a8858b839104f85bfc6fd607432da85e27a58ec52"
+            url: "https://github.com/ivasic/google-maps-ios/releases/download/4.2.0/GooglePlaces.xcframework.zip",
+            checksum: "6de987fa5f3eb0e19fc614a7228504ebe05462df30f93d67a7d8c9b29896f155"
         )
     ]
 )
